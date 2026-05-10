07854282e2a5dcff795b78bde718e3e477f1db01
Udvab Biswas <biswasudvab14@gmail.com>
2026-05-10 13:35:01 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://UDVAB:u2108t1401@cluster0.bnf5zyi.mongodb.net/foodle').then(() => console.log("Connected to MongoDB")).catch((err) => console.log(err));
+    await mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB")).catch((err) => console.log(err));
 }
